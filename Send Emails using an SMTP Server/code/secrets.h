d2e854685b2798766885a0f91ca457c9f55ce102
IEEE Projects Bengaluru <105450161+IEEEProjectsBengaluru@users.noreply.github.com>
2023-05-07 16:20:08 +0530

    Add files via upload

---
+++ Send Emails using an SMTP Server/code/secrets.h
@@ -0,0 +1,15 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
+
+#define SMTP_HOST "smtp.office365.com"
+#define SMTP_PORT 587
+
+/* The sign in credentials */
+#define AUTHOR_EMAIL "OUTLOOKEMAIL"
+#define AUTHOR_PASSWORD "PASSWORD"
+
+/* Recipient's email*/
+#define RECIPIENT_EMAIL "EMAILID"
